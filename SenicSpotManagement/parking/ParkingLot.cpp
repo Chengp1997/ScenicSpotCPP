008d7a63d39801fcd975eac2484338e558f2e4cf
zhfan901 <frank_f@infi.us>
2023-01-26 22:36:47 -0500

    fixing map bugs and some add ons

--- SenicSpotManagement/parking/ParkingLot.cpp
+++ SenicSpotManagement/parking/ParkingLot.cpp
@@ -12,7 +12,8 @@ ParkingLot::ParkingLot(int size) { this->size = size;}
 
 void ParkingLot::park(string carId, string arriveTime) {
     Car toEnter(carId, arriveTime);
-    //check if already exist
+    Car currCar = toEnter;
+        // check if already exist
     auto iter = record.find(carId);
     if (record.find(carId)==record.end()){ //already exist
         if(iter->second.getStatus() == Car::INLOT){//in parking lot
@@ -21,24 +22,45 @@ void ParkingLot::park(string carId, string arriveTime) {
         }
     }
 
-    record.insert(carId, toEnter);
+    record.insert(std::make_pair(carId, currCar));
     //not exist, input the car
     for(int i=0;i<parkingSpace.size();i++){
         //if parking lot is full
         if (parkingSpace.size() == this->size){
-            cout<< "******Parking Log is Full, go to waiting line****"<<endl;
+            cout<< "******Parking Lot is Full, go to waiting line****"<<endl;
             waitingLine.push(toEnter);
 
         }else{
-
+            // get curr time 之后给分出来 太他妈丑了
+            time_t rawtime;
+            time(&rawtime);
+            currCar.setStartTime(ctime(&rawtime));
+            //
+            parkingSpace.push(currCar);
+            cout << "******New car" + carId + "entered****" << endl;
         }
     }
 }
 
 void ParkingLot::leave(string carId, string leaveTime) {
+    //find already exists id
 
+    auto iter = record.find(carId);
+    Car currCar = iter->second;
+
+                  // if the car doesn't exist
+    if (record.find(carId) != record.end())
+    {
+        cout<<"Invalid Car ID, Police on the way"<<endl;
+    }
+    time_t rawtime;
+    time(&rawtime);
+    currCar.setStatus(Car::INLOT); // gone status
+    currCar.setEndTime(ctime(&rawtime));
+    parkingSpace.pop();
 }
 
 void ParkingLot::showParkingLot() {
 
 }
+
