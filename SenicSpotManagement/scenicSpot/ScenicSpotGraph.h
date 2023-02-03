b1a5e791e6c35b650ff8064109919b3b3e022ac7
Chengp1997 <593330227@qq.com>
2023-02-03 01:59:53 -0500

    function update - init method and adjacency initial method update

--- SenicSpotManagement/scenicSpot/ScenicSpotGraph.h
+++ SenicSpotManagement/scenicSpot/ScenicSpotGraph.h
@@ -6,7 +6,9 @@
 #define SENICSPOTMANAGEMENT_SCENICSPOTGRAPH_H
 #include "ScenicSpotVertex.h"
 #include "../utils/common/FileProcessor.h"
+#include "../utils/common/Utils.h"
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -15,14 +17,18 @@ private:
     string EdgesPath = "files/spots.txt";
     string VertexPath = "files/spotsInfo.txt";
     LinkedList<ScenicSpotVertex> nodes;
+    map<string, ScenicSpotVertex> spotMap;//for better find vertex
     int** adjacencyMatrix ;
 
     void initGraph();//initialize graph to adjacency matrix
+    void initSpotsInfo(const vector<string>& spotInfo);
+    void initEdgeInfo(const vector<string>& edgeInfo);
     void toAdjacencyMatrix();
     void printMatrix();
 public:
     ScenicSpotGraph(){
         initGraph();
+        toAdjacencyMatrix();
     };
 
     void printGraph();//print current adjacency matrix
