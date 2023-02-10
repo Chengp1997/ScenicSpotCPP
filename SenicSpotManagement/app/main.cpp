d6b3f6d5dd50546ada3252f58b945a7f5bba01fb
Chengp1997 <593330227@qq.com>
2023-02-10 02:06:20 -0500

    Finish main -- testing

--- SenicSpotManagement/app/main.cpp
+++ SenicSpotManagement/app/main.cpp
@@ -1,7 +1,15 @@
+#include <vector>
 #include "Menu.h"
+#include "../parking/Car.h"
+#include "../utils/dataStructure/LinkedList.h"
+#include "../utils/common/FileProcessor.h"
+#include "../scenicSpot/ScenicSpotGraph.h"
+#include "../utils/algorithm/ShortestPath.h"
+#include "../utils/algorithm/MST.h"
+
 int main() {
-    Graph graph ;
-    Menu::printMenu(graph);
+    Menu menu;
+    menu.printMenu();
 }
 
 
